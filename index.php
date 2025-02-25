8a4d335e8f1d6befac28088522fa2dbab875632e
sudaox01 <suda@mail.sudaox.com>
2025-02-24 20:08:15 -0600

    Suck my ASS git

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 // Fragments Main
 // if no fragment is selected
@@ -30,37 +29,4 @@ if($_GET['f'] == "show_hub") {
 if($_GET['f'] == "show_hub_sessions") {
     require("fragments/show_hub_sessions.php");
     die();
-=======
-<?php 
-// Fragments Main
-// if no fragment is selected
-if(!isset($_GET['f'])) {
-    header("Location: ?f=admin");
-}
-// fragments
-// admin
-if($_GET['f'] == "admin") {
-    require("fragments/admin.php");
-    die();
-}
-// login
-if($_GET['f'] == "login") {
-    require("fragments/login.php");
-    die();
-}
-// logout
-if($_GET['f'] == "logout") {
-    require("fragments/logout.php");
-    die();
-}
-// show_hub
-if($_GET['f'] == "show_hub") {
-    require("fragments/show_hub.php");
-    die();
-}
-// show_hub_sessions
-if($_GET['f'] == "show_hub_sessions") {
-    require("fragments/show_hub_sessions.php");
-    die();
->>>>>>> 26b8f4e921bf54ba1ab4248fe78457aa8bb234dd
 }
